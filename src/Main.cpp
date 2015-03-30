3f7d5865958fe66753c65e459a38c6748d869050
James McLean <james@mcleanlabs.ca>
2015-03-29 22:46:57 -0400

    More tweaks.

--- src/Main.cpp
+++ src/Main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char** argv) {
 	std::string vsFile = prefix + "/lambertian.vs";
 	std::string fsFile = prefix + "/lambertian.fs";
 	
-	ShadersPtr shaders = ShadersPtr(new Shaders(vsFile, fsFile));
+	ShadersPtr shaders(new Shaders(vsFile, fsFile));
 	shaders->setSamplerName("texMap");
 	
 	model->loadTextures();
